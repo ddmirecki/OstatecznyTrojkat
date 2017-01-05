cc10ae609492f675ae2a21ce5b09dbee0af3701f
marcinbluj <marcinbluj21@gmail.com>
2017-01-05 20:17:50 +0100

    ostatecznytrojkat

--- src/main/java/pl/ostatecznyTrojkat/Math.java
+++ src/main/java/pl/ostatecznyTrojkat/Math.java
@@ -1,13 +1,24 @@
 package pl.ostatecznyTrojkat;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by RENT on 2017-01-05.
  */
 public class Math {
-    public static List<Factors> getFactors(int number){
-
-        return null;
+    public static List<Factors> getFactors(int number) {
+        if (number == 0) {
+            throw new IllegalArgumentException();
+        } else {
+            List<Factors> list = new ArrayList<Factors>();
+            for (int i = 1; i <= java.lang.Math.abs(number); i++) {
+                if (number % i == 0) {
+                    int secondNumber = number / i;
+                    list.add(new Factors(i, secondNumber));
+                }
+            }
+            return list;
+        }
     }
 }
