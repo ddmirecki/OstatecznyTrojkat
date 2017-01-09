4f810f8fc1e3443dc7e28ce5ee46edaada9a3a3e
Jakub Superson <kubasuperson@gmail.com>
2017-01-09 18:27:39 +0100

    nowe zmiany

--- src/test/java/pl/ostatecznyTrojkat/MathTest.java
+++ src/test/java/pl/ostatecznyTrojkat/MathTest.java
@@ -1,7 +1,30 @@
 package pl.ostatecznyTrojkat;
 
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 /**
  * Created by RENT on 2017-01-05.
  */
 public class MathTest {
+    @Test
+    public void getFactors_simple() {
+
+        // nowy komentarz
+        List<Factors> factors = Math.getFactors(10);
+
+        assertTrue(factors.contains(new Factors(10, 1)));
+
+        assertEquals(factors.size(), 4);
+        assertEquals(factors.get(0).getX(), 1);
+        assertEquals(factors.get(0).getY(), 10);
+
+        assertEquals(factors.get(3).getX(), 10);
+        assertEquals(factors.get(3).getY(), 1);
+
+    }
 }
