c5c6784e94f8f78c7443f408094b487b9b1dc8e4
marcinbluj <marcinbluj21@gmail.com>
2017-01-09 18:27:32 +0100

    ostatecznytrojkat

--- src/main/java/pl/ostatecznyTrojkat/Math.java
+++ src/main/java/pl/ostatecznyTrojkat/Math.java
@@ -12,7 +12,7 @@ public class Math {
             throw new IllegalArgumentException();
         } else {
             List<Factors> list = new ArrayList<Factors>();
-            for (int i = 1; i <= java.lang.Math.abs(number); i++) {
+            for (int i = 1; i <= java.lang.Math.abs(number); i++) { //dla wartosci ujemnych
                 if (number % i == 0) {
                     int secondNumber = number / i;
                     list.add(new Factors(i, secondNumber));
